b50948c23693b1b6a48c02e98de4af621d160b9e
Roho7 <rohosen2@gmail.com>
2023-06-22 08:59:32 +0530

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import { Center, Square, Circle, Flex, Box, Spacer } from "@chakra-ui/react";
+
+function Navbar() {
+  return <Flex bg="#909090" p="2" justify="between"></Flex>;
+}
+
+export default Navbar;
