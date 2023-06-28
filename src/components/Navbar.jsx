a482f9ef5eb6aaafdf5db195f4876d75b0067883
Roho7 <rohosen2@gmail.com>
2023-06-28 21:21:49 +0530

    toolips

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,21 @@
 import React from "react";
 import { Routes, Route, Link } from "react-router-dom";
 import { PhoneIcon, StarIcon } from "@chakra-ui/icons";
-import { AiFillHome, AiTwotoneProject } from "react-icons/ai";
+import { AiFillHome, AiFillProject } from "react-icons/ai";
 
 function Navbar() {
   return (
-    <div className="absolute bottom-0 w-screen z-10 flex justify-center gap-8 p-8 rounded-xl text-skin-500 ">
-      <Link to="/" className="flex flex-col items-center">
+    <div className="absolute bottom-0 w-screen z-10 flex justify-center gap-16 p-8 rounded-xl text-skin-500 ">
+      <Link to="/" className="flex flex-col items-center group">
+        <div className="tooltip">Home</div>
         <AiFillHome />
       </Link>
-      <Link to="/projects" className="flex flex-col items-center">
-        <AiTwotoneProject />
+      <Link to="/projects" className="flex flex-col items-center group">
+        <div className="tooltip">Projects</div>
+        <AiFillProject />
       </Link>
-      <Link to="/contact" className="flex flex-col items-center">
+      <Link to="/contact" className="flex flex-col items-center group">
+        <div className="tooltip">Contact</div>
         <PhoneIcon />
       </Link>
     </div>
